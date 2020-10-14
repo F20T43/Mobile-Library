497c4e5a06012e6f0d7d68f45026f4ccbb1d3f2a
XXX3CCC <weixi@ualberta.ca>
2020-10-14 15:40:49 -0600

    Mobile Library 1.2

--- MobileLibrary/app/src/main/java/com/example/mobilelibrary/BorrowerLogin.java
+++ MobileLibrary/app/src/main/java/com/example/mobilelibrary/BorrowerLogin.java
@@ -2,13 +2,28 @@ package com.example.mobilelibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class BorrowerLogin extends AppCompatActivity {
+
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrower_login);
+
+        back = (ImageButton) findViewById(R.id.b_back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(BorrowerLogin.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
